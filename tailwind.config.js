7119670671a48659042b31eec1506b3844b76109
calogero_messina <c.messina97engineer@gmail.com>
2025-01-11 02:20:33 +0100

    feat: replace bootstrap with tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,27 @@
+const { fontFamily, colors } = require("tailwindcss/defaultTheme");
+const Unfonts = require("unplugin-fonts");
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      nunito: ["Nunito", ...fontFamily.sans],
+      heebo: ["Heebo", ...fontFamily.sans],
+    },
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1440px",
+    },
+    extend: {
+      colors: {
+        primary: "#fea116",
+        dark: "#0F172B",
+        white: "#fff",
+      },
+    },
   },
-  plugins: [require('@tailwindcss/forms')],
-  
-}
-
+  plugins: [require("@tailwindcss/forms")],
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@hooks": path.resolve(__dirname, "./src/hooks"),
+      "@pages": path.resolve(__dirname, "./src/pages"),
+      "@router": path.resolve(__dirname, "./src/router"),
+      "@scss": path.resolve(__dirname, "./src/scss"),
+      "@services": path.resolve(__dirname, "./src/services"),
+      "@stores": path.resolve(__dirname, "./src/stores"),
+    },
+  },
+});
