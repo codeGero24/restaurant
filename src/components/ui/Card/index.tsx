c98b7c191773bd4a3c7b4640bb539df13d0e0a4b
calogero_messina <c.messina97engineer@gmail.com>
2025-01-15 17:37:19 +0100

    feat: component Card

--- src/components/ui/Card/index.tsx
+++ src/components/ui/Card/index.tsx
@@ -1,21 +1,45 @@
 import React from 'react';
-interface CardProps {
+import { clsx } from 'clsx';
+import CardHeader from './CardHeader';
+import CardTitle from './CardTitle';
+import CardSubtitle from './CardSubtitle';
+import CardContent from './CardContent';
+import CardFooter from './CardFooter';
+import './Card.scss';
+
+interface CardProps extends React.HTMLAttributes<HTMLDivElement> {
+  title?: string;
+  subtitle?: string;
+  description?: string;
   className?: string;
   children?: React.ReactNode;
-  title?: string;
-  description?: string;
 }
 
-const Card = React.forwardRef<HTMLDivElement, CardProps>(
-  ({ title, description, className, children }, ref) => {
-    return (
-      <div ref={ref} className={className}>
-        {children}
-        <h5>{title}</h5>
-        <p>{description}</p>
-      </div>
-    );
-  }
-);
+const Card = ({ className, title, subtitle, children, ...props }: CardProps) => {
+  return (
+    <div
+      className={clsx(
+        'bck-card',
+        'grid cursor-pointer grid-cols-1 gap-2 rounded-md bg-white p-4 shadow-sm',
+        'md:p-6',
+        className,
+      )}
+      {...props}
+    >
+      {children}
+      {title && <CardTitle title={title} />}
+      {subtitle && <CardSubtitle subtitle={subtitle} />}
+    </div>
+  );
+};
 
-export default Card;
+Card.displayName = 'Card';
+
+export default {
+  Root: Card,
+  Header: CardHeader,
+  Title: CardTitle,
+  Subtitle: CardSubtitle,
+  Content: CardContent,
+  Footer: CardFooter,
+};
