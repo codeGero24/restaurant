293df0489d92af2a6bba9446d55d7902b7fd4680
calogero_messina <c.messina97engineer@gmail.com>
2025-01-15 18:11:11 +0100

    feat: update page Service

--- src/pages/Service/index.tsx
+++ src/pages/Service/index.tsx
@@ -1,44 +1,25 @@
 import Section from '@components/layout/Section';
-import { services } from '@mock/services';
-import Card from '@components/ui/Card';
-import Icon from '@components/ui/Icon';
-import { clsx } from 'clsx';
+import CardService, { type Service } from '@components/ui/Card/Service';
+import AnimationWrapper from '@components/layout/AnimationWrapper';
+
+// - MOCK DATA
+import MOCK_DATA from '@mock/index';
 
 export default function ServicePage() {
+  const { services } = MOCK_DATA;
+
   return (
     <>
-      <Section className={clsx('px-5 py-5 md:py-12 lg:gap-12')}>
-        <div className={clsx('section__header', 'pb-12 text-center')}>
-          <h1>Explore Our Services</h1>
-        </div>
-
-        <div
-          className={clsx(
-            'section__content',
-            'grid grid-cols-1 gap-4',
-            'md:grid-cols-2 md:gap-8',
-            'lg:grid-cols-4'
-          )}
-        >
-          {services.map(service => (
-            <Card
-              key={service.title}
-              {...service}
-              className={clsx('grid grid-cols-1 gap-2 rounded-md bg-white p-4 shadow-sm')}
-            >
-              <Icon {...service.icon} />
-            </Card>
-          ))}
-          {services.map(service => (
-            <Card
-              key={service.title}
-              {...service}
-              className={clsx('grid grid-cols-1 gap-2 rounded-md bg-white p-4 shadow-sm')}
-            >
-              <Icon {...service.icon} />
-            </Card>
-          ))}
-        </div>
+      <Section title="Services" subTitle="Our Services">
+        {services.concat(services).map((service: Service, index: number) => (
+          <AnimationWrapper
+            key={`service-${index}`}
+            animation="animate-slide-in-up"
+            delay={true}
+          >
+            <CardService service={service} />
+          </AnimationWrapper>
+        ))}
       </Section>
     </>
   );
