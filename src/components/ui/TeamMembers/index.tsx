618a2370728785f986e1d9dd780e3af49d21b32f
calogero_messina <c.messina97engineer@gmail.com>
2025-01-15 17:53:45 +0100

    feat: component TeamMembers

---
+++ src/components/ui/TeamMembers/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import Section from '@components/layout/Section';
+import AnimationWrapper from '@components/layout/AnimationWrapper';
+import MemberCard, { type Member } from '@components/ui/Card/Member';
+
+import MOCK_DATA from '@mock/index';
+
+interface TeamMembersProps {
+  members?: Member[];
+}
+
+const TeamMembers: React.FC<TeamMembersProps> = React.memo(({ members }) => {
+  const teamMembers = members || (MOCK_DATA.members as Member[]);
+
+  return (
+    <Section title="Team Members" subTitle="Our Team Members">
+      {teamMembers.map((member, index) => (
+        <AnimationWrapper
+          key={`member-${index}`}
+          animation="animate-slide-in-up"
+          delay={true}
+        >
+          <MemberCard member={member} />
+        </AnimationWrapper>
+      ))}
+    </Section>
+  );
+});
+
+export default TeamMembers;
